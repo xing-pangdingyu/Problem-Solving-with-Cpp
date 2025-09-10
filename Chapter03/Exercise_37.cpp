357fed9d6df15be7c5e8a3c9c59eb783fa17d2fc
Xing Pangdingyu <114491466+xing-pangdingyu@users.noreply.github.com>
2025-09-10 18:29:30 +0800

    Add Self-Test Exercise 37

---
+++ Chapter03/Exercise_37.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <limits>
+
+int read_even() {
+    int value = 0;
+
+    while (true) {
+        std::cout << "Input an even number: ";
+
+        if ((std::cin >> value) && value % 2 == 0) {
+            return value;
+        }
+        std::cerr << "Invalid input.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
+
+int main()
+{
+	int sum = 0;
+
+    while (true) {
+        int even_num = read_even();
+
+        sum += even_num;
+
+        if (even_num == 0) { // Sentinel value = 0
+            break;
+        }
+    }
+
+	std::cout << "Sum = " << sum << ".\n";
+
+	return 0;
+}
