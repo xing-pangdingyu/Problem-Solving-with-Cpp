a7fceaeedcff0352cb63dfc1eca1f86a015fa93a
Xing Pangdingyu <114491466+xing-pangdingyu@users.noreply.github.com>
2025-09-14 15:14:37 +0800

    Add Programming Project 9

---
+++ Chapter03/Project_09.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <random>
+#include <iomanip>
+
+bool win_monty_hall(int original, int prize, bool change_door) {
+    return change_door ? (original != prize) : (original == prize);
+}
+
+int main() {
+    constexpr int trials = 10000;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> door_dist(1, 3);
+
+    int win_change = 0; int win_keep = 0;
+    for (int i = 0; i < trials; ++i) {
+        int original = door_dist(gen);
+        int prize = door_dist(gen);
+        if (win_monty_hall(original, prize, true)) win_change++;
+        if (win_monty_hall(original, prize, false)) win_keep++;
+    }
+
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Change choice: " << 100.0 * win_change / trials << "%\n"
+              << "Keep choice: " << 100.0 * win_keep / trials << "%\n";
+
+	return 0;
+}
